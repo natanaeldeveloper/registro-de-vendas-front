5269d0ee468a63cc91f2ee93c5b5620e549b2947
Natanael Oliveira <natanael.martins@fabricainfo.com>
2024-09-20 18:01:18 -0300

    refactor: tentativa de criação de uma nova interface

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,46 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const drawer = ref(false)
-
-const toggleDrawer = () => {
-  drawer.value = !drawer.value
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <v-layout>
-    <v-app-bar color="primary">
-      <template v-slot:prepend>
-        <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
-        <v-app-bar-title>Gestão de vendas</v-app-bar-title>
-      </template>
-      <template v-slot:append>
-        <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
-    </v-app-bar>
-    <v-navigation-drawer v-model="drawer">
-      <v-list>
-        <v-list-item title="Home"></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-main>
-      <RouterView />
-    </v-main>
-  </v-layout>
+  <main>
+    <header class="d-flex ga-3 w-100 justify-space-between py-4 px-4">
+      <div class="d-flex ga-4 align-center">
+        <v-avatar color="success">NA</v-avatar>
+        <h6 class="text-h6 text-white">Natanael</h6>
+      </div>
+      <div>
+        <v-btn icon="mdi-magnify" variant="plain" color="white"></v-btn>
+        <v-btn icon="mdi-bell-outline" variant="plain" color="white"></v-btn>
+      </div>
+    </header>
+    <v-card color="transparent">
+      <v-card-text>
+        <v-slide-group>
+          <v-slide-group-item v-for="n in 1" :key="n" v-slot="{ toggle, isSelected }">
+            <v-card
+              v-for="n in 5"
+              :key="n"
+              :color="
+                ['#9529FF', '#FB8500', '#8EDF34', '#4CC9F0', '#B8001C'].find(
+                  (_, index) => n - 1 === index
+                )
+              "
+              width="350"
+              :height="isSelected ? '240' : '250'"
+              class="rounded-xl mx-1"
+              @click="toggle"
+            >
+              <v-card-text>Options {{ n }} {{ isSelected }} </v-card-text>
+            </v-card>
+          </v-slide-group-item>
+        </v-slide-group>
+      </v-card-text>
+    </v-card>
+  </main>
 </template>
+
+<style scoped>
+main {
+  background-color: #131313;
+  min-height: 100vh;
+}
+</style>
