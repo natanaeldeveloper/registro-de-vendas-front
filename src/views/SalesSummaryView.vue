8320b9410b44061f63632a6318e4b8e59995a51b
Natanael Oliveira <natanael.martins@fabricainfo.com>
2024-09-23 13:23:49 -0300

    feat: adicionado selects de ano e mês na tela de resumo das vendas

--- src/views/SalesSummaryView.vue
+++ src/views/SalesSummaryView.vue
@@ -1,6 +1,13 @@
 <script lang="ts" setup>
 import Navbar from '@/components/salesSummary/navbar/navbar.vue'
+import { ref } from 'vue'
 import { useRoute } from 'vue-router'
+
+const year = ref('2024')
+const years = ['2020', '2021', '2022', '2023', '2024']
+
+const month = ref('Agosto')
+const months = ['Janeiro', 'Fervereiro', 'Março', 'Abril', 'Maio']
 
 const { params } = useRoute()
 const id = params['id']
@@ -9,8 +16,46 @@ const id = params['id']
 <template>
   <v-container>
     <Navbar />
-    <div>
-      <h1>Summary {{ id }}</h1>
+    <div class="d-flex flex-column justify-space-center mt-4">
+      <div class="mx-auto">
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-btn size="large" color="contrast" variant="text" v-bind="props">
+              {{ year }}
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in years"
+              :key="index"
+              :value="item"
+              @click="year = item"
+            >
+              <v-list-item-title class="text-center">{{ item }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+      <div class="mx-auto">
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-btn size="large" color="contrast" variant="text" v-bind="props">
+              {{ month }}
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in months"
+              :key="index"
+              :value="item"
+              @click="month = item"
+            >
+              <v-list-item-title class="text-center">{{ item }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
     </div>
+    <div></div>
   </v-container>
 </template>
